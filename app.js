804f3923f239e1d4692725baa1bb5e8e048aa176
Adam <jchernick2010@gmail.com>
2019-03-31 00:12:21 -0700

    clean up
    clean up

--- app.js
+++ app.js
@@ -34,6 +34,46 @@ app.post('/send', (req, res) => {
         <h3>Message</h3>
         <p>${req.body.message}</p>
     `;
+
+    // Generate test SMTP service account from ethereal.email
+    // Only needed if you don't have a real mail account for testing
+    // let account = await nodemailer.createTestAccount();
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: "StrollAppEmail@gmail.com", // generated ethereal user
+            pass: "Shot51222!" // generated ethereal password
+        },
+        tls:{
+            rejectUnauthorized: false
+        }
+    });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Stroll" <StrollAppEmail@gmail.com>', // sender address
+        to: "jchernick2010@gmail.com", // list of receivers
+        subject: "Hello ✔", // Subject line
+        text: "Hello world?", // plain text body
+        html: output // html body
+    };
+
+    // send mail with defined transport object
+    // let info = await transporter.sendMail(mailOptions)
+    transporter.sendMail(mailOptions, (error, info) => {
+        if(error){
+            return console.log(error);
+        }
+
+        console.log("Message sent: %s", info.messageId);
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+        res.render('contact', {msg: 'Email has been sent'});
+    });
 });
 
 app.listen(3000, () => console.log("server started..."))
